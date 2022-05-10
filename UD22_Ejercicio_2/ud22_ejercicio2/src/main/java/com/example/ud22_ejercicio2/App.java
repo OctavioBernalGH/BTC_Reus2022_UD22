fbe22e5b821ea199f171d25a958e702e81ec69e7
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-10 11:24:39 +0200

    Se sube estructura ejercicio 2

---
+++ UD22_Ejercicio_2/ud22_ejercicio2/src/main/java/com/example/ud22_ejercicio2/App.java
@@ -0,0 +1,13 @@
+package com.example.ud22_ejercicio2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
