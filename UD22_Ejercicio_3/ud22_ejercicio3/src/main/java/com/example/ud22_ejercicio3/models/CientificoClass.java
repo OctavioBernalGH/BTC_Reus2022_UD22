9084cf13b6a1f73e5f1aa399e284259c05dedef3
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-17 12:11:22 +0200

    Creacion CientificoClass

--- UD22_Ejercicio_3/ud22_ejercicio3/src/main/java/com/example/ud22_ejercicio3/models/CientificoClass.java
+++ UD22_Ejercicio_3/ud22_ejercicio3/src/main/java/com/example/ud22_ejercicio3/models/CientificoClass.java
@@ -1,5 +1,36 @@
 package com.example.ud22_ejercicio3.models;
 
-public class CientificoClass {
+import java.util.Date;
 
+public class CientificoClass {
+	/* Atributos de clase ClienteClass */
+	private String dni;
+	private String nomApels;
+	
+	
+	public CientificoClass() {
+		
+	}
+	
+	public CientificoClass(String dni, String nomApels) {
+		super();
+		this.dni = dni;
+		this.nomApels = nomApels;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNomApels() {
+		return nomApels;
+	}
+
+	public void setNomApels(String nomApels) {
+		this.nomApels = nomApels;
+	}
 }
