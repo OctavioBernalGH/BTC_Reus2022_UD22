40e7cb5e737e7ca1e64990b67da8e3ddf936f3bb
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-17 12:13:43 +0200

    Creacion ProyectoClass

--- UD22_Ejercicio_3/ud22_ejercicio3/src/main/java/com/example/ud22_ejercicio3/models/ProyectoClass.java
+++ UD22_Ejercicio_3/ud22_ejercicio3/src/main/java/com/example/ud22_ejercicio3/models/ProyectoClass.java
@@ -1,5 +1,45 @@
 package com.example.ud22_ejercicio3.models;
 
 public class ProyectoClass {
+	/* Atributos de clase ClienteClass */
+	private String id;
+	private String nombre;
+	private int horas;
+	
+	public ProyectoClass() {
+		
+	}
+	
+	public ProyectoClass(String id, String nombre, int horas) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.horas = horas;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	
+	
 }
